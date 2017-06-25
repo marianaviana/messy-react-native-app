1c7e8e67b3a0becdddc3e5e4cb5505fa60588e96
Thiago Cifani <cifani.thiago@gmail.com>
2017-06-26 06:48:57 +0800

    Remove classes nao utilizadas.

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { Scene, Router, Actions } from 'react-native-router-flux';
+import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import Dashboard from './components/Dashboard';
 import GymCard from './components/GymCard';
+import GymCardShow from './components/GymCardShow';
+import Assessment from './components/Assessment';
+import AssessmentShow from './components/AssessmentShow';
 
 const RouterComponent = () => {
   return (
@@ -12,11 +16,24 @@ const RouterComponent = () => {
 
       <Scene key="main">
         <Scene
+          key="Dashboard"
+          component={Dashboard}
+          title="Dashboard"
+          initial
+        />
+        <Scene
           key="GymCard"
           component={GymCard}
           title="Ficha de Treino"
-          initial
         />
+        <Scene key="gymcardShow" component={GymCardShow} title="Detalhes da Ficha" />
+
+        <Scene
+          key="assessment"
+          component={Assessment}
+          title="Avaliação"
+        />
+        <Scene key="assessmentShow" component={AssessmentShow} title="Detalhes da Avaliação" />
       </Scene>
     </Router>
   );
