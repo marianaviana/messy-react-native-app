da3fbbfc9e8469d934188dbf5d8b1d4fa7ac931e
Luiza Melo <meloluiza@live.com>
2017-06-27 02:29:41 -0300

    Ajusta ICQ

--- src/components/Icq.js
+++ src/components/Icq.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import { Card } from 'react-native-material-design';
 import { Text, View } from 'react-native';
-import Imc from './Imc';
 
 class Icq extends Component {
 
   render() {
-    const { icq, icqMessage } = this.props.assessment.icq;
-    const { imc, message } = this.props.assessment.imc;
+    const { icq, message } = this.props.assessment.icq;
     return (
       <View>
-        <Imc assessment={this.props.assessment} />
         <Card>
           <Card.Body>
             <Text>Icq: { icq }</Text>
-            <Text>Mensagem: { icqMessage }</Text>
-            <Text>Resultado: O imc é { imc } e o resultado foi { message }</Text>
+            <Text>Mensagem: { message }</Text>
           </Card.Body>
         </Card>
       </View>
